59794eaccffd4f8eba4299601717d3da417b6046
SalmanA2321 <learnerdude9@gmail.com>
2023-03-21 15:17:58 +0500

    Header updated a little bit

--- my-project/src/Components/Header.jsx
+++ my-project/src/Components/Header.jsx
@@ -32,59 +32,59 @@ const Header = () => {
         // onClick={onClick}
         // selectedKeys={[current]}
         mode="horizontal"
-        className="menu shadow h-fit flex flex-wrap my-2 justify-evenly"
+        className="menu border-b border-[#D9D9D9] h-fit flex flex-wrap items-center py-3 justify-evenly"
       >
+        {/* Logo */}
         <Item>
           <Link to="/">
-            <img className="h-30 w-[10rem] " src="Logo.png" alto="" />
+            <img className="h-30 w-[10rem] " src="Logo.svg" alto="" />
           </Link>
         </Item>
+
+        {/* Search */}
         <Item>
           <input
-            className=" border-none font-sans  text-xl outline-none text-[#248F59]"
+            className=" border border-[#D9D9D9] font-sans  text-base outline-none h-[3.4375rem] w-[23.4375rem] px-3 rounded-lg text-[#248F59]"
             type="search"
-            placeholder="Search Shops..."
+            placeholder="Search..."
           />
         </Item>
 
+        {/* Delivering to */}
         <Item>
-          <p className=" flex flex-wrap font-sans font-semibold text-[#248F59]">
+          <div className=" flex items-center h-[3.4375rem] font-sans font-semibold text-base text-[#248F59]">
             Delivering to:
-            <img className="h-2/6 my-auto" src="Location on.png" alt="" />
-          </p>
+            <img className="h-2/6 my-auto" src="location.svg" alt="" />
+            <div className="flex">
+              <p className="w-[150px] text-ellipsis overflow-hidden text-[#808080] font-normal">Neewan Bazar Ghakkhar, Gujranwala</p>
+              <div>
+                <img src="down.svg" />
+              </div>
+            </div>
+          </div>
+
         </Item>
-        <Item key="register">
-          <Link to="/Signup">
-            <button className="h-9 mb-2 w-[7.4375rem] align-middle text-white bg-[#248F59] rounded-lg font-sans uppercase font-semibold">
-            <UserAddOutlined className="align-middle" style={{fontSize:"1.1rem"}}/> Sign Up
-            </button>
+        
+        {/* Account */}
+        <Item>
+          <Link>
+            <img className="w-8" src="Account.svg" alt="" />
           </Link>
         </Item>
 
-        <Item key="login">
-          <Link to="/Login">
-            <p className="font-sans  mx-auto text-[#248F59]">
-              <LoginOutlined className="align-middle" /> Login
-            </p>
-          </Link>
-        </Item>
-
+        {/* Cart */}
         <Item>
           <Link>
-            <img src="Account circle.png" alt="" />
+            <img className="w-8" src="Cart.svg" alt="" />
           </Link>
         </Item>
-        <Item>
-          <Link>
-            <img src="Shopping cart.png" alt="" />
-          </Link>
-        </Item>
-        <SubMenu title="User">
+        
+        {/* <SubMenu title="User">
           <Item key="dashboard" icon={<DashboardTwoTone />}>
             <Link to="/dashboard">Dashboard</Link>
           </Item>
           <Item icon={<LogoutOutlined />}>LogOut</Item>
-        </SubMenu>
+        </SubMenu> */}
       </Menu>
     
   );
