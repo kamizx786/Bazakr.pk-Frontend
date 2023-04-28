449a7d9d4873ff6e6ef5d434e94c3e3b9578ac40
Kamran Ali <Kamran Ali@KAMRAN-ALI>
2023-04-29 00:27:46 +0500

    First

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Drawer,Modal } from 'antd';
+import { Button, Drawer, Modal } from "antd";
 import { useState } from "react";
 import Login from "../Pages/Auth/Login";
 import Register from "../Pages/Auth/Register";
 import CartSidebarView from "./cart_sidebar_viewer";
-import {ShoppingBagIcon,PhoneXMarkIcon} from "@heroicons/react/24/solid"
-
+import { ShoppingBagIcon, PhoneXMarkIcon ,Bars3Icon} from "@heroicons/react/24/solid";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
@@ -14,7 +13,7 @@ const Header = () => {
   const [openModal, setOpenModal] = useState(false);
   const [authCon, setauthCon] = useState(false);
 
-//handle Drawer
+  //handle Drawer
   const showDrawer = () => {
     setOpen(true);
   };
@@ -23,140 +22,152 @@ const Header = () => {
     setOpen(false);
     setOpencart(false);
   };
-//Handle Modal
-const handleModal=()=>{
-setOpenModal(true);
-setOpen(false)
-}
-const handleCart=()=>{
-  setOpencart(true);
-  setOpen(false)
-  }
+  //Handle Modal
+  const handleModal = () => {
+    setOpenModal(true);
+    setOpen(false);
+  };
+  const handleCart = () => {
+    setOpencart(true);
+    setOpen(false);
+  };
   return (
     <>
-    <header className="flex items-center justify-between px-10 border-b border-[#D9D9D9]">
-      <Link to="/">
-            <img className="h-30 w-[10rem] " src="Logo.svg" alto="" />
-      </Link>
+      <header className="flex items-center justify-between px-10 border-b border-[#D9D9D9]">
+        <Link to="/">
+          <img className="h-30 w-[10rem] " src="Logo.svg" alto="" />
+        </Link>
 
-      <div
-        // onClick={onClick}
-        // selectedKeys={[current]}
-        mode="horizontal"
-        className="menu h-fit sm:hidden hidden md:flex gap-3 flex-wrap items-center py-3  justify-evenly"
-      >
-        <div>
-        {/* Search */}
-        
-          <input
-            className=" border border-[#D9D9D9] font-sans  text-base outline-none h-[3.4375rem] w-[23.4375rem] px-3 rounded-lg text-[#248F59]"
-            type="search"
-            placeholder="Search..."
-          />
-       
-        </div>  
-        <div >
-          <Link to="/shops" className="text-muted text-lg">
-            Shop
-          </Link>
-          </div>
-          
-          <div >
-          <Link to="/contact" className="text-muted text-lg" >
-            Contact
-          </Link>
-          </div>
-        {/* Account */}
-       <div onClick={handleModal}>
-          <Link >
-            <img className="w-8" src="Account.svg" alt="" />
-          </Link>
-          </div>
+        <div
+          // onClick={onClick}
+          // selectedKeys={[current]}
+          mode="horizontal"
+          className="menu h-fit sm:hidden hidden md:flex gap-3 flex-wrap items-center py-3  justify-evenly"
+        >
+          <div>
+            {/* Search */}
 
-        {/* Cart */}
-       <div onClick={handleCart}>
-          <Link>
-            <img className="w-8" src="Cart.svg" alt="" />
-          </Link>
+            <input
+              className=" border border-[#D9D9D9] font-sans  text-base outline-none h-[3.4375rem] w-[23.4375rem] px-3 rounded-lg text-[#248F59]"
+              type="search"
+              placeholder="Search..."
+            />
           </div>
           <div>
-          <button className="bg-[#248F59] text-white py-3 px-3 rounded">
-          Become a Seller
-          </button>
-        </div>
-        {/* <SubMenu title="User">
+            <Link to="/shops" className="text-muted text-lg">
+              Shop
+            </Link>
+          </div>
+
+          <div>
+            <Link to="/contact" className="text-muted text-lg">
+              Contact
+            </Link>
+          </div>
+          {/* Account */}
+          <div onClick={handleModal}>
+            <Link>
+              <img className="w-8" src="Account.svg" alt="" />
+            </Link>
+          </div>
+
+          {/* Cart */}
+          <div onClick={handleCart}>
+            <Link>
+              <img className="w-8" src="Cart.svg" alt="" />
+            </Link>
+          </div>
+          <div>
+            <button className="bg-[#248F59] text-white py-3 px-3 rounded">
+              Become a Seller
+            </button>
+          </div>
+          {/* <SubMenu title="User">
           <Item key="dashboard" icon={<DashboardTwoTone />}>
             <Link to="/dashboard">Dashboard</Link>
           </Item>
           <Item icon={<LogoutOutlined />}>LogOut</Item>
         </SubMenu> */}
-      </div>
-      
-      <div className="md:hidden
-      menu h-fit  flex-wrap items-center py-3 justify-evenly
-      " >
-          <Button   onClick={showDrawer}>
-            <img src="../Menu.svg" className="w-8 " />
-          </Button>
-          <Drawer title="Mobile Menu" placement="right" onClose={onClose} open={open}>
-            <div className="flex flex-col gap-3">
-            {/* Search */}
-            <div>
-              <input
-                className=" border border-[#D9D9D9] font-sans  text-base outline-none h-[3.4375rem] px-3 rounded-lg text-[#248F59]"
-                type="search"
-                placeholder="Search..."
-              />
-            </div>
-            <div >
-          <Link to="/shops" onClick={onClose}
-           className="flex gap-3 items-center text-base
-          text-muted ">
-                 <ShoppingBagIcon className="w-8 text-[#248F59]" />
-                <div className="font-sans text-[#00000080]">Shop</div>
-          </Link>
-          </div>
-          <div >
-          <Link to="/contact" onClick={onClose}
-           className="text-muted flex gap-3 items-center text-base" >
-          <PhoneXMarkIcon  className="w-8 text-[#248F59]"/>
-                <div className="font-sans text-[#00000080]">Contact</div>
-          </Link>
-          </div>
-            {/* Account */}
-            <div  onClick={handleModal}>
-              <Link className="flex gap-3 items-center text-base">
-                <img className="w-8" src="Account.svg" alt="" />
-                <div className="font-sans text-[#00000080]">Account</div>
-              </Link>
-            </div>
+        </div>
 
-            {/* Cart */}
-            <div onClick={handleCart}>
-              <Link className="flex gap-3 items-center text-base">
-                <img className="w-8" src="Cart.svg" alt="" />
-                <div className="font-sans text-[#00000080]">Cart</div>
-              </Link>
-            </div>
+        <div
+          className="md:hidden menu h-fit  flex-wrap items-center py-3 justify-evenly" >
+          <Button onClick={showDrawer} className="pb-2">
+            <Bars3Icon  className="h-8 pb-2 text-[#248F59]" />
+          </Button>
+          <Drawer
+            title="Mobile Menu"
+            placement="right"
+            onClose={onClose}
+            open={open}
+          >
+            <div className="flex flex-col gap-3">
+              {/* Search */}
+              <div>
+                <input
+                  className=" border border-[#D9D9D9] font-sans  text-base outline-none h-[3.4375rem] px-3 rounded-lg text-[#248F59]"
+                  type="search"
+                  placeholder="Search..."
+                />
+              </div>
+              <div>
+                <Link
+                  to="/shops"
+                  onClick={onClose}
+                  className="flex gap-3 items-center text-base
+          text-muted "
+                >
+                  <ShoppingBagIcon className="w-8 text-[#248F59]" />
+                  <div className="font-sans text-[#00000080]">Shop</div>
+                </Link>
+              </div>
+              <div>
+                <Link
+                  to="/contact"
+                  onClick={onClose}
+                  className="text-muted flex gap-3 items-center text-base"
+                >
+                  <PhoneXMarkIcon className="w-8 text-[#248F59]" />
+                  <div className="font-sans text-[#00000080]">Contact</div>
+                </Link>
+              </div>
+              {/* Account */}
+              <div onClick={handleModal}>
+                <Link className="flex gap-3 items-center text-base">
+                  <img className="w-8" src="Account.svg" alt="" />
+                  <div className="font-sans text-[#00000080]">Account</div>
+                </Link>
+              </div>
+
+              {/* Cart */}
+              <div onClick={handleCart}>
+                <Link className="flex gap-3 items-center text-base">
+                  <img className="w-8" src="Cart.svg" alt="" />
+                  <div className="font-sans text-[#00000080]">Cart</div>
+                </Link>
+              </div>
             </div>
           </Drawer>
         </div>
-
       </header>
-{/* Handlecart Drawer */}
-<Drawer
- title={null} onClose={onClose} open={openCart} placement="right">
-<CartSidebarView setOpencart={setOpencart}/>
-</Drawer>
- {/* Modal For login and Signup */}
-<Modal 
-footer={null}
-closable={false}
- open={openModal} onCancel={()=>setOpenModal(false)}>
-{!authCon?<Login setauthCon={setauthCon} setOpenModal={setOpenModal}/>:
-<Register setauthCon={setauthCon}/>}
-</Modal>
-</>
+      {/* Handlecart Drawer */}
+      <Drawer title={null} onClose={onClose} open={openCart} placement="right">
+        <CartSidebarView setOpencart={setOpencart} />
+      </Drawer>
+      {/* Modal For login and Signup */}
+      <Modal
+        footer={null}
+        closable={false}
+        open={openModal}
+        onCancel={() => setOpenModal(false)}
+      >
+        {!authCon ? (
+          <Login setauthCon={setauthCon} setOpenModal={setOpenModal} />
+        ) : (
+          <Register setauthCon={setauthCon} />
+        )}
+      </Modal>
+    </>
   );
 };
 
