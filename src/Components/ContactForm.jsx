5d35846ca095308a3100dfc8f2335de0aee70410
Kamran Ali <Kamran Ali@KAMRAN-ALI>
2023-04-29 00:14:20 +0500

    First

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -5,18 +5,18 @@ const ContactForm = () => {
     <form >
           <div className="flex flex-col gap-2 ">
           {/* Field */}
-          <div className='flex flex-wrap gap-5 mb-2'>
-        <div>
+          <div className='"grid grid-cols-2 mb-2'>
+        <div className='flex flex-col mb-2'>
           <h4 className="font-sans mb-2">Email</h4>
           <input
             type="text"
             id="email"
             name="email"
             placeholder="Enter Your Email"
-            className="focus:outline-none focus:ring-2 focus:ring-green-600 h-12  w-[18rem] px-3 border rounded-lg border-neutral-300"
+            className="focus:outline-none focus:ring-2 focus:ring-green-600 h-12  px-3 border rounded-lg border-neutral-300"
           />
           </div>
-          <div>
+          <div className='flex flex-col mb-2'>
            <h4 className="font-sans mb-2">Name</h4>
           <input
             type="text"
@@ -24,7 +24,7 @@ const ContactForm = () => {
             name="Name"
             placeholder="Enter Your Name"
             className="focus:outline-none
-             focus:ring-2 focus:ring-green-600 h-12 w-[18rem] px-3 border rounded-lg border-neutral-300"
+             focus:ring-2 focus:ring-green-600 h-12  px-3 border rounded-lg border-neutral-300"
           />
           </div>
 
@@ -49,30 +49,7 @@ const ContactForm = () => {
           Submit
           </button>
         </div>
-          {/* <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-            <label>Name</label>
-            <input type='text'/>
-            <label>Email</label>
-            <input type="email"/>
-          </div> */}
-          {/* <Input
-            label={t('text-subject')}
-            {...register('subject')}
-            variant="outline"
-            className="my-6"
-            error={t(errors.subject?.message!)}
-          />
-          <TextArea
-            label={t('text-description')}
-            {...register('description')}
-            variant="outline"
-            className="my-6"
-            error={t(errors.description?.message!)}
-          />
-
-          <Button loading={isLoading} disabled={isLoading}>
-            {t('text-submit')}
-          </Button> */}
+         
         
     </form>
   )
