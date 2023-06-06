627c9f747731015bb8f21e59bb4b5b09fbb70627
Kamran Ali <Kamran Ali@KAMRAN-ALI>
2023-06-06 23:34:34 +0500

    prices

--- src/Pages/Products/function.jsx
+++ src/Pages/Products/function.jsx
@@ -8,7 +8,11 @@ export const handleCart = (p, dispatch, setOpen) => {
     const existingProduct = cart.findIndex(item => item._id === p._id);
     if (existingProduct !== -1) {
       // Increase the quantity of the existing product by 1
+     if(cart[existingProduct].order_quantity +1>cart[existingProduct].quantity){
+      toast.error("Max Stock Reached");
+     }else{
       cart[existingProduct].order_quantity += 1;
+     }
     } else if (cart.length > 0 && cart[0].store._id !== p.store._id) {
         toast("You cannot add products from different shops to the cart.Please Remove Others First");
       }  else {
